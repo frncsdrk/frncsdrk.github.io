a8f128bf92fc9076540e66cdad12cfa40417a915
frncsdrk <carlsmoh@gmail.com>
2018-02-28 19:29:54 +0100

    Move page back to root

--- vendor.sh
+++ vendor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p ./app/vendor/spectre.css/dist
-cp -r ./node_modules/spectre.css/dist/* ./app/vendor/spectre.css/dist
+mkdir -p ./vendor/spectre.css/dist
+cp -r ./node_modules/spectre.css/dist/* ./vendor/spectre.css/dist
 echo "DONE"
