ad4b792b27fbe502b600c7bbc02accc46780535a
frncsdrk <carlsmoh@gmail.com>
2018-11-14 23:47:29 +0100

    Update spectre.css

--- vendor.sh
+++ vendor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p ./vendor/spectre.css/dist
-cp -r ./node_modules/spectre.css/dist/* ./vendor/spectre.css/dist
+cp -r ./node_modules/spectre.css/dist/* ./vendor/spectre.css
 echo "DONE"
