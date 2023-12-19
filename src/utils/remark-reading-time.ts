49df46ef25e1e0b35bf5a94c8f7781bcc2e500cf
frncsdrk <git@frncsdrk.me>
2023-12-19 10:42:01 +0100

    [fix] Fix TS errors

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -2,6 +2,8 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
+    // Disabling ts check for the following line only
+    // @ts-expect-error
 	return function (tree, { data }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
